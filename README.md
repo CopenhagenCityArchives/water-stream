5a50d5756f1aa7fb69439ef1e85196701bda5182
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-11-05 19:40:01 +0100

    Initial README and a simple test

---
+++ README.md
@@ -0,0 +1,5 @@
+Water Stream
+===
+The `water-stream` package combines one image ontop of another without the use of libraries that reads from or writes to the disk.
+
+The creek used for tests is created by Dan Hetteix (https://thenounproject.com/search/?q=creek&i=22411)
---
+++ test/memory.js
@@ -0,0 +1,15 @@
+// const heapdump = require('heapdump');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const waterStream = require('../transformation');
+
+const watermarkPath = path.join(__dirname, 'watermarks', 'creek-500.png');
+const watermarkBuffer = fs.readFileSync(watermarkPath);
+
+const server = http.createServer((req, res) => {
+  http.get('http://placekitten.com/g/600/600', (catRes) => {
+    let transformation = waterStream.transformation(watermarkBuffer);
+    catRes.pipe(transformation).pipe(res);
+  });
+}).listen(1337, '127.0.0.1');
